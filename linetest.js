fe9f958994b4bfe6e5db86d87a32627b0c8834a1
pecu <pecu610@gmail.com>
2016-11-16 18:00:21 +0800

    Update linetest.js

--- linetest.js
+++ linetest.js
@@ -10,7 +10,7 @@ var valueline = d3.line()
     .x(function(d) { return x(d.date); })
     .y(function(d) { return y(d.close); });
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select(".content").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
