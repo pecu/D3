0bc8028565147ee707961334a1081e226dddd1b0
pecu <pecu610@gmail.com>
2016-11-22 13:21:01 +0800

    2016-11-22 test.js file path

--- test.js
+++ test.js
@@ -3,9 +3,9 @@ function test()
 	var width = 1024;
 	var height = 768;
 	var ctrl = d3.select(".newsletter").append("svg").attr("width", width).attr("height", height);
-	d3.csv("stocks.csv",
+	d3.csv("https://pecu.github.io/D3/stocks.csv",
 	      function(data) { console.log(data); });
-	d3.csv("./HistoricalQuotes.csv", 
+	d3.csv("https://pecu.github.io/D3/HistoricalQuotes.csv", 
 		function(data)
 		{
 			var ln = data.length;
