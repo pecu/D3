5e1aed1a2244c53326ad27830515c55e3a389c1f
pecu <pecu610@gmail.com>
2016-11-29 13:42:08 +0800

    add word js file

---
+++ myWD.js
@@ -0,0 +1,12 @@
+function myWD()
+{
+d3.csv("https://pecu.github.io/D3/wc.csv", 
+      function(data)
+      {
+        d3.wordcloud()
+          .size([500, 300])
+          .fill(d3.scale.ordinal().range(["#884400", "#448800", "#888800", "#444400"]))
+          .words(data)
+          .start();
+      });
+}
